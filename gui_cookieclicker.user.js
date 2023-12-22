a7af7c8db7995541c88fe5dab02c2cbeb46f8e0a
supercoolbro <121699538+wowletsgo@users.noreply.github.com>
2023-12-21 17:19:38 -0800

    Update gui_cookieclicker.user.js

--- gui_cookieclicker.user.js
+++ gui_cookieclicker.user.js
@@ -6,7 +6,7 @@
 // @description  a really nice cookier clicker hack.
 // @author       zcheat
 // @match        https://orteil.dashnet.org/cookieclicker/
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=gxcorner.games
+// @icon         https://play-lh.googleusercontent.com/Z1MOuuiD05ZN5LkVmMEvKF0mqAc-FknaQ2j8s4dZiO-LSPQX4EEA3RVJdlQEtxe96ok
 // @updateURL    https://github.com/wowletsgo/zcheat_LOL/raw/main/gui_cookieclicker.user.js
 // @downloadURL  https://github.com/wowletsgo/zcheat_LOL/raw/main/gui_cookieclicker.user.js
 // @grant        none
