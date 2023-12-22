d2d29b6d3747cfe60bf146eebb6e3efc242c8ce8
supercoolbro <121699538+wowletsgo@users.noreply.github.com>
2023-12-21 17:04:36 -0800

    Update gui_undetectedsearchproxy.js

--- gui_undetectedsearchproxy.js
+++ gui_undetectedsearchproxy.js
@@ -1,1 +1,1 @@
-
+// If you are having trouble using this script, Just go to https://www.tampermonkey.net and download the extension and on the dashboard create a new script and paste the code below.
