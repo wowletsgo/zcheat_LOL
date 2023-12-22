57b8d84a3abad257d10998842461157f987506ce
supercoolbro <121699538+wowletsgo@users.noreply.github.com>
2023-12-21 17:17:48 -0800

    Update gui_cookieclicker.user.js

--- gui_cookieclicker.user.js
+++ gui_cookieclicker.user.js
@@ -1,1 +1,19 @@
-// If you are having trouble using this script, Just go to https://www.tampermonkey.net and download the extension and on the dashboard create a new script and paste the code below.
+// If you are having trouble using this script, Just go to https://www.tampermonkey.net and download the extension and on the dashboard create a new script and paste the code below. also do not include this first line when pasting.
+
+// ==UserScript==
+// @name         zcheat_cookieclicker
+// @version      0.6
+// @description  a really nice cookier clicker hack.
+// @author       zcheat
+// @match        https://orteil.dashnet.org/cookieclicker/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=gxcorner.games
+// @updateURL    https://github.com/wowletsgo/zcheat_LOL/raw/main/gui_cookieclicker.user.js
+// @downloadURL  https://github.com/wowletsgo/zcheat_LOL/raw/main/gui_cookieclicker.user.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
